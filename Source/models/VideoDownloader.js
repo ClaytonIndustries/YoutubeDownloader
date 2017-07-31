a5bcd41ecb59f5b16d8e78de1374b9c1bfe8cd6a
Michael Clayton <michael.clayton@sky.uk>
2017-07-31 10:10:45 +0100

    Determine if a video failed to download

--- Source/models/VideoDownloader.js
+++ Source/models/VideoDownloader.js
@@ -7,19 +7,18 @@ export default class VideoDownloader {
                 callback("size", httpRequest.getResponseHeader("Content-Length"));
             }          
         }
-        httpRequest.onprogress = (evt) => {
-            callback("progress", evt.loaded);
-        }
         httpRequest.onload = () => {
-            let byteArray = new Uint8Array(httpRequest.response);
-            callback("complete", byteArray);
+            if(httpRequest.status == 200) {
+                let byteArray = new Uint8Array(httpRequest.response);
+                callback("complete", byteArray);
+            }
+            else {
+                callback("error");
+            }
         }
-        httpRequest.onerror = () => {
-            callback("error");
-        }
-        httpRequest.onabort = () => {
-            callback("error");
-        };
+        httpRequest.onprogress = (evt) => callback("progress", evt.loaded);
+        httpRequest.onerror = () => callback("error");
+        httpRequest.onabort = () => callback("error");
         httpRequest.open("GET", url, true);
         httpRequest.responseType = "arraybuffer";
         httpRequest.send();
