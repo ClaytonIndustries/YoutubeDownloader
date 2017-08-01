cebc9e6eee13320d60074552ff2db9b42f8f8e10
Michael Clayton <mike-16@hotmail.co.uk>
2017-08-01 11:52:42 +0100

    Working parse on client

--- Source/models/YoutubeUrlParser.js
+++ Source/models/YoutubeUrlParser.js
@@ -2,32 +2,29 @@ import { URL_PARSE, AUTH_CODE } from './Constants';
 
 export default class YoutubeUrlParser {
     parse(youtubeUrl, callback) {
-        try {
-            this.downloadWebpage(youtubeUrl, (result) => {
-                if(result) {
-                    let title = this.extractTitle(result);
-                    let fmtStreamMapSection = decodeURIComponent(this.extractAdaptiveFmtSection(result));
-                    let adaptiveFmtSection = decodeURIComponent(this.extractFmtStreamMapSection(result));
-                    let playerUrl = this.extractplayerUrl(result);
-                    let videoId = new RegExp("v=(.*)").exec(youtubeUrl)[1];
-
-                    this.extractUrls(title, fmtStreamMapSection, adaptiveFmtSection, playerUrl, videoId, (urls) => {
-                        if(urls) {
-                            callback(true, urls);
-                        }
-                        else {
-                            callback(false, null);
-                        }
-                    });
-                }
-                else {
-                    callback(false, null);
-                }
-            });
-        }
-        catch(e) {
-            callback(false, null);
-        }
+        this.downloadWebpage(youtubeUrl, (webpage) => {
+            if(webpage) {
+                let webpageData = this.extractWebpageData(webpage, youtubeUrl);
+                this.downloadPlayer(webpageData.playerUrl, (crypto) => {
+                    if(crypto) {
+                        this.downloadQualities(webpageData, crypto, (urls) => {
+                            if(urls) {
+                                callback(true, urls);
+                            }
+                            else {
+                                callback(false, null);
+                            }
+                        });
+                    }
+                    else {
+                        callback(false, null);
+                    }
+                });
+            }
+            else {
+                callback(false, null);
+            }
+        });
     }
 
     downloadWebpage(youtubeUrl, callback) {
@@ -45,12 +42,48 @@ export default class YoutubeUrlParser {
         httpRequest.send();
     }
 
-    extractUrls(title, fmtStreamMapSection, adaptiveFmtSection, playerUrl, videoId, callback) {
+    downloadPlayer(playerUrl, callback) {
         let httpRequest = new XMLHttpRequest();
         httpRequest.onload = () => {
             if(httpRequest.status == 200) {
-                let response = JSON.parse(httpRequest.responseText);
-                callback(response);
+                try {
+                    let webpage = httpRequest.responseText;
+
+                    let cryptoClassFunctionCalls = this.getCryptoClassFunctionCallsFromCryptoFunction(webpage);
+                    let mappedCryptoFunctions = this.getCrytoClassFunctions(webpage, cryptoClassFunctionCalls[0]);
+
+                    let crypto = {
+                        cryptoClassFunctionCalls: cryptoClassFunctionCalls,
+                        mappedCryptoFunctions: mappedCryptoFunctions
+                    };
+
+                    callback(crypto);
+                }
+                catch(e) {
+                    callback();
+                }
+            }
+            else {
+                callback();
+            }
+        };
+        httpRequest.onerror = () => callback();
+        httpRequest.open("GET", playerUrl, true);
+        httpRequest.send();
+    }
+
+    downloadQualities(webpageData, crypto, callback) {
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.onload = () => {
+            if(httpRequest.status == 200) {
+                try {
+                    let response = JSON.parse(httpRequest.responseText);
+                    callback(response);
+                }
+                catch(e) {
+                    callback();
+                }
+
             }
             else {
                 callback();
@@ -61,12 +94,76 @@ export default class YoutubeUrlParser {
         httpRequest.setRequestHeader("Authorization", AUTH_CODE);
         httpRequest.setRequestHeader("Content-type", "application/json");
         httpRequest.send(JSON.stringify({
-            title: title,
-            fmtStreamMapSection: fmtStreamMapSection,
-            adaptiveFmtSection: adaptiveFmtSection,
-            playerUrl: playerUrl,
-            videoId: videoId
+            title: webpageData.title,
+            fmtStreamMapSection: webpageData.fmtStreamMapSection,
+            adaptiveFmtSection: webpageData.adaptiveFmtSection,
+            videoId: webpageData.videoId,
+            crypto: crypto
         }));
+    }
+
+    getCryptoClassFunctionCallsFromCryptoFunction(webpage) {       
+        let functionBody = new RegExp("{([a-z]=[a-z]\\.split\\(\"\"\\).*?;return [a-z]\\.join\\(\"\"\\))}").exec(webpage)[1];
+
+		let cryptoFunctionNames = functionBody.split(";");
+
+		for(let i = 0; i < cryptoFunctionNames.length; i++) {
+			cryptoFunctionNames[i] = cryptoFunctionNames[i].replace("\n", "");
+		}
+
+		cryptoFunctionNames.shift();
+		cryptoFunctionNames.pop();
+
+		return cryptoFunctionNames;
+	}
+
+	getCrytoClassFunctions(webpage, anyCryptoClassFunctionCall) {
+        let cryptoClassName = new RegExp(".+\\.").exec(anyCryptoClassFunctionCall)[0];
+
+		cryptoClassName = cryptoClassName.replace('.', '');
+
+		let cryptoClass = new RegExp("var " + cryptoClassName + "={([\\s\\S]*?)};").exec(webpage)[1];
+
+		let cryptoClassFunctions = cryptoClass.split("},");
+
+		for(let i = 0; i < cryptoClassFunctions.length; i++) {
+			cryptoClassFunctions[i] = cryptoClassFunctions[i].replace("\n", "");
+		}
+
+		return this.mapCryptoClassFunctionsToOperations(cryptoClassFunctions);
+	}
+
+	mapCryptoClassFunctionsToOperations(cryptoClassFunctions) {
+        let functions = [];
+        
+        for(let i = 0; i < cryptoClassFunctions.length; i++) {
+            if(cryptoClassFunctions[i].includes('splice')) {
+				functions.push({func: cryptoClassFunctions[i].substr(0, 2), action: 'splice'});
+			}
+			else if(cryptoClassFunctions[i].includes('reverse')) {
+				functions.push({func: cryptoClassFunctions[i].substr(0, 2), action: 'reverse'});
+			}
+			else {
+				functions.push({func: cryptoClassFunctions[i].substr(0, 2), action: 'swap'});
+			}
+		}
+
+		return functions;
+    }
+    
+    extractWebpageData(webpage, youtubeUrl) {
+        try {
+            return {
+                title: this.extractTitle(webpage),
+                fmtStreamMapSection: this.extractAdaptiveFmtSection(webpage),
+                adaptiveFmtSection: this.extractFmtStreamMapSection(webpage),
+                playerUrl: this.extractplayerUrl(webpage),
+                videoId: new RegExp("v=(.*)").exec(youtubeUrl)[1]
+            };
+        }
+        catch(e) {
+            return undefined;
+        }
     }
 
     extractTitle(webpage) {
@@ -74,17 +171,17 @@ export default class YoutubeUrlParser {
     }
 
     extractFmtStreamMapSection(webpage) {
-        return new RegExp("\"url_encoded_fmt_stream_map\":\"([\\s\\S]*?)\",").exec(webpage)[1];
+        return decodeURIComponent(new RegExp("\"url_encoded_fmt_stream_map\":\"([\\s\\S]*?)\",").exec(webpage)[1]);
     }
 
     extractAdaptiveFmtSection(webpage) {
-        return new RegExp("\"adaptive_fmts\":\"([\\s\\S]*?)\",").exec(webpage)[1];
+        return decodeURIComponent(new RegExp("\"adaptive_fmts\":\"([\\s\\S]*?)\",").exec(webpage)[1]);
     }
 
     extractplayerUrl(webpage) {
         let playerUrl = new RegExp("\"js\":(.*?).js\"").exec(webpage)[0];
-        playerUrl = playerUrl.replace("/", "");
-        playerUrl = playerUrl.replace("\\", "");
+        playerUrl = playerUrl.replace(/"/g, "");
+        playerUrl = playerUrl.replace(/\\/g, "");
         playerUrl = playerUrl.replace(" ", "");
         playerUrl = playerUrl.replace("js:", "https://www.youtube.com");
         return playerUrl;
